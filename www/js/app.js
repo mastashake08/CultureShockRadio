217a3651e2df0a83c020e0a865d4ba7bf39742ca
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-13 06:59:27 -0700

    Ads and song titles

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,27 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic','ngCordova'])
 
-.run(function($ionicPlatform, $cordovaNativeAudio) {
+.run(function($ionicPlatform, $cordovaNativeAudio, $rootScope, $http) {
   $ionicPlatform.ready(function() {
+    // select the right Ad Id according to platform
+    var admobid = {};
+    if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos
+        admobid = {
+            banner: 'ca-app-pub-7023023584987784/3437106151', // or DFP format "/6253334/dfp_example_ad"
+            interstitial: 'ca-app-pub-7023023584987784/3936633751'
+        };
+    } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+        admobid = {
+            banner: 'ca-app-pub-7023023584987784/6890100150', // or DFP format "/6253334/dfp_example_ad"
+            interstitial: 'ca-app-pub-7023023584987784/8366833355'
+        };
+    }
+    
+    if(AdMob) AdMob.createBanner( {
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    autoShow: true,
+    isTesting: true} );
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -28,6 +47,17 @@ angular.module('starter', ['ionic','ngCordova'])
      console.error(error);
    });
    $cordovaNativeAudio.loop('music');
+   $rootScope.trackTitle = '';
+   $rootScope.url = 'http://90kradio.com:8000/json.xsl';
+   $rootScope.getJson = function(){
+     $http.get($rootScope.url).success(function(data){
+      console.log(data);
+   })};
+
+   setTimeout(function(){$rootScope.getJson()},1000);
 
   });
+})
+.controller('HomeCtrl', function($http, $rootScope, setTimeout){
+
 })
